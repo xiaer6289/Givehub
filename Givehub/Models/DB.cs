3f4ce86d64306869bc39f8782cc875f9dec39f1b
xiaer6289 <164221150+xiaer6289@users.noreply.github.com>
2025-11-11 23:42:40 +0800

    basic database filed created

--- Givehub/Models/DB.cs
+++ Givehub/Models/DB.cs
@@ -1,14 +1,101 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 namespace Givehub.Models
 {
     public class DB : DbContext
     {
-        public DB(DbContextOptions<DB> options) : base(options)
-        {
+        public DB(DbContextOptions<DB> options) : base(options) { }
 
-        }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Admin> Admins { get; set; }
+        public DbSet<Payment> Payments { get; set; }
+        public DbSet<Donee> Donees { get; set; }
 
     }
     
-    
+    public class User
+    {
+        [Key]
+        [Required]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string email { get; set; }
+
+        [Required]
+        [MaxLength(11)]
+        [RegularExpression(@"^01[0-9]{8,13}$", ErrorMessage = "Phone number must start with '01' and be 10 to 15 digits long.")]
+        public string phoneNo { get; set; }
+
+        [MaxLength(200)]
+        [Required]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,20}$",
+        ErrorMessage = "Password must be 8 to 20 characters long, with at least one uppercase letter, one lowercase letter, one digit, and one special character (!@#$%^&*).")]
+        public string password { get; set; }
+    }
+
+    public class Admin
+    {
+        [Key]
+        [Required]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string email { get; set; }
+
+        [MaxLength(200)]
+        [Required]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,20}$",
+        ErrorMessage = "Password must be 8 to 20 characters long, with at least one uppercase letter, one lowercase letter, one digit, and one special character (!@#$%^&*).")]
+        public string password { get; set; }
+    }
+
+    public class Payment
+    {
+        [Key]
+        [Required]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [MaxLength(20)]
+        [Required]
+        public string method { get; set; }
+
+        [Precision(10, 2)]
+        [Required]
+        public decimal amount { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd HH:mm}")]
+        [Required]
+        public DateTime date { get; set; }
+
+        public string? StripeTransactionId { get; set; }
+    }
+
+    public class Donee
+    {
+        [Key]
+        [Required]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public string id { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd HH:mm}")]
+        [Required]
+        public DateTime date { get; set; }
+
+        public string? description { get; set; }
+
+        public string? image { get; set; }
+
+        public string? requirements { get; set; }
+    }
 }
