bf5c5ec2a007442e7190db22b23e5de9052e5fc5
xiaer6289 <164221150+xiaer6289@users.noreply.github.com>
2025-11-11 10:36:15 +0800

    basic setups

---
+++ Givehub/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Givehub.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
