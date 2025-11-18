139cb3adcb31b0382e4062d9307ff57bff848f3f
YC <cherng.yw@gmail.com>
2025-11-18 11:09:02 +0800

    admin and donee create page only

---
+++ Givehub/Controllers/DoneeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Givehub.Controllers;
+
+public class DoneeController : Controller
+{
+
+    public IActionResult DoneeHomePage()
+    {
+        return View();
+    }
+
+}
