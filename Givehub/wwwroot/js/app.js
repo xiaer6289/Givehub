704685d54618ceb7d9cf2dc2a64333d92b95f0fc
Max-code0101 <siewchunfeng2006@gmail.com>
2025-12-01 14:10:50 +0800

    Items donation page done

--- Givehub/wwwroot/js/app.js
+++ Givehub/wwwroot/js/app.js
@@ -1,1 +1,32 @@
 ﻿
+//----------------Item Page End ---------------------//
+
+
+
+// "Add More" button will add a identical input form for donation entry
+document.querySelector(".add-more-button").addEventListener("click", function (e) {
+
+    e.preventDefault(); // Prevent the page from reloading
+
+    const wrapper = document.getElementById("donation-wrapper");
+    const firstForm = document.querySelector(".donation-entry");
+    const newForm = firstForm.cloneNode(true);
+
+    newForm.querySelector("input").value = "";
+
+    // Insert the new form before the button section**
+    const buttonSection = document.querySelector(".button-section");
+    const dateSection = document.querySelector(".delivery-date");
+
+    wrapper.insertBefore(newForm, dateSection);
+});
+
+// "Remove" button will remove the respective donation entry form
+document.addEventListener("click", function (e) {
+    if (e.target.closest(".remove-donation-button")) {
+        const row = e.target.closest(".donation-entry");
+        row.remove();
+    }
+})
+
+//-----------------Item Page End---------------------//
